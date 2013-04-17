0721e2223abe4a92384e1016968299aabc1b2800
Dana Kaplan <danamkaplan@gmail.com>
2013-04-16 20:23:25 -0700

    created object class to be any abstract object in the game

--- tutorial.py
+++ tutorial.py
@@ -1,11 +1,11 @@
 import libtcodpy as libtcod
-import os
  
 #actual size of the window
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
  
 LIMIT_FPS = 20  #20 frames-per-second maximum
+ 
  
 class Object:
     #this is a generic object: the player, a monster, an item, the stairs...
@@ -15,21 +15,21 @@ class Object:
         self.y = y
         self.char = char
         self.color = color
-
+ 
     def move(self, dx, dy):
-        #Move by the given amount
+        #move by the given amount
         self.x += dx
         self.y += dy
-
+ 
     def draw(self):
         #set the color and then draw the character that represents this object at its position
         libtcod.console_set_default_foreground(con, self.color)
         libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
-
+ 
     def clear(self):
         #erase the character that represents this object
         libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
-
+ 
  
 def handle_keys():
     #key = libtcod.console_check_for_keypress()  #real-time
@@ -44,54 +44,51 @@ def handle_keys():
  
     #movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        player.move(0,-1)
+        player.move(0, -1)
  
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        player.move(0,1)
+        player.move(0, 1)
  
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
         player.move(-1, 0)
  
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        player.move(1,0)
-
-
-
+        player.move(1, 0)
+ 
+ 
 #############################################
 # Initialization & Main Loop
 #############################################
-
-font = os.path.join('data','fonts', 'arial10x10.png') 
-libtcod.console_set_custom_font(font, libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+ 
+libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
 libtcod.sys_set_fps(LIMIT_FPS)
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-
-#create the player object  
+ 
+#create object representing the player
 player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
-
-#create the npc object
-npc = Object(SCREEN_WIDTH/2-5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
-#list of both objects
+ 
+#create an NPC
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+ 
+#the list of objects with those two
 objects = [npc, player]
-
-
-
+ 
+ 
 while not libtcod.console_is_window_closed():
-
-    #draw all objects in objects list
+ 
+    #draw all objects in the list
     for object in objects:
         object.draw()
-
-    #blit the contents of "con" to the root console and present it   
+ 
+    #blit the contents of "con" to the root console and present it
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
     libtcod.console_flush()
  
-    #erase all objects at their old locations before they move
+    #erase all objects at their old locations, before they move
     for object in objects:
         object.clear()
-
+ 
     #handle keys and exit game if needed
     exit = handle_keys()
     if exit:
